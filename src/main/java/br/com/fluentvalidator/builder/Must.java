88af463eaa5d06e889d0f584187f693c6f3cb54b
mvallim <mvallim@lenovo.localdomain>
2019-10-16 00:58:45 -0300

    Remove duplicate code

--- src/main/java/br/com/fluentvalidator/builder/Must.java
+++ src/main/java/br/com/fluentvalidator/builder/Must.java
@@ -1,11 +1,8 @@
 package br.com.fluentvalidator.builder;
 
-import java.util.function.Function;
 import java.util.function.Predicate;
-import br.com.fluentvalidator.exception.ValidationException;
-import br.com.fluentvalidator.handler.HandlerInvalidField;
 
-public interface Must<T, P, W extends When<T, P, W>> {
+public interface Must<T, P, W extends When<T, P, W>> extends When<T, P, W> {
 
   /**
    *
@@ -13,81 +10,5 @@ public interface Must<T, P, W extends When<T, P, W>> {
    * @return
    */
   When<T, P, W> when(final Predicate<P> when);
-
-  /**
-   *
-   * @param code
-   * @return
-   */
-  Code<T, P, W> withCode(final String code);
-
-  /**
-   *
-   * @param code
-   * @return
-   */
-  Code<T, P, W> withCode(final Function<T, String> code);
-
-  /**
-   *
-   * @param message
-   * @return
-   */
-  Message<T, P, W> withMessage(final String message);
-
-  /**
-   *
-   * @param message
-   * @return
-   */
-  Message<T, P, W> withMessage(final Function<T, String> message);
-
-  /**
-   *
-   * @param fieldName
-   * @return
-   */
-  FieldName<T, P, W> withFieldName(final String fieldName);
-
-  /**
-   *
-   * @param fieldName
-   * @return
-   */
-  FieldName<T, P, W> withFieldName(final Function<T, String> fieldName);
-
-  /**
-   *
-   * @param fieldName
-   * @return
-   */
-  AttemptedValue<T, P, W> withAttempedValue(final P attemptedValue);
-
-  /**
-   *
-   * @param fieldName
-   * @return
-   */
-  AttemptedValue<T, P, W> withAttempedValue(final Function<T, P> attemptedValue);
-
-  /**
-   *
-   * @return
-   */
-  Critical<T, P, W> critical();
-
-  /**
-   *
-   * @param clazz
-   * @return
-   */
-  Critical<T, P, W> critical(final Class<? extends ValidationException> clazz);
-
-  /**
-   *
-   * @param handlerInvalidField
-   * @return
-   */
-  HandleInvalidField<T, P, W> handlerInvalidField(final HandlerInvalidField<P> handlerInvalidField);
 
 }
