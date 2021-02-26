ded36208fd01008f0e6dc72c3ee8ea48f0aa36d5
Marcos Tischer Vallim <tischer@gmail.com>
2021-02-26 19:29:22 -0300

    Update AbstractValidator.java

--- src/main/java/br/com/fluentvalidator/AbstractValidator.java
+++ src/main/java/br/com/fluentvalidator/AbstractValidator.java
@@ -22,26 +22,49 @@ public abstract class AbstractValidator<T> implements Validator<T> {
 
   private final List<Rule<T>> rules = new LinkedList<>();
 
-  private final Runnable initialize;
+  private final Initializer<T> initialize;
 
   private String property;
 
   private RuleProcessorStrategy ruleProcessor = RuleProcessorStrategy.getDefault();
 
-  protected AbstractValidator() {
-    this.initialize = new Runnable() {
+  private static class Initializer<T> {
 
-      private boolean initialized;
+    private final AtomicReference<Boolean> atomicReference = new AtomicReference<>(Boolean.FALSE);
 
-      @Override
-      public synchronized void run() {
-        if (!initialized) {
-          rules();
-          this.initialized = true;
+    private final Validator<T> validator;
+
+    Initializer(final Validator<T> validator) {
+      this.validator = validator;
+    }
+
+    /**
+     * This method cause Race Condition. We are using Compare And Swap (CAS)
+     * <p>
+     * {@link https://en.wikipedia.org/wiki/Race_condition} 
+     * {@link https://en.wikipedia.org/wiki/Compare-and-swap}
+     */
+    public void init() {
+      if (isNotInitialized()) {
+        synchronized (atomicReference) {
+          if (isNotInitialized()) { // double check if was initialized
+            validator.rules();
+            final Boolean oldValue = atomicReference.get();
+            final Boolean newValue = Boolean.TRUE;
+            atomicReference.compareAndSet(oldValue, newValue);
+          }
         }
       }
+    }
 
-    };
+    private boolean isNotInitialized() {
+      return Boolean.FALSE.equals(atomicReference.get());
+    }
+
+  }
+  
+  protected AbstractValidator() {
+    this.initialize = new Initializer<T>();
   }
 
   /**
@@ -114,7 +137,7 @@ public abstract class AbstractValidator<T> implements Validator<T> {
    */
   @Override
   public boolean apply(final T instance) {
-    this.initialize.run();
+    this.initialize.init();
     ValidationContext.get().setProperty(this.property, instance);
     return ruleProcessor.process(instance, instance, rules);
   }
